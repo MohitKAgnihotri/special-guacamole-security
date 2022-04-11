fd9e7f4dfd4624e71917f28f3f2bcf03fe3b183c
Mohit Agnihotri <mohit.agnihotri@signify.com>
2022-04-11 10:59:49 +0200

    Fixed few testcases and added comments

--- mvalloc.c
+++ mvalloc.c
@@ -1,6 +1,7 @@
 #include "mvalloc.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
 #include "mem_alloc_types.h"
 
 /* pointer to the beginning of the memory region to manage */
@@ -17,6 +18,7 @@ enum ALGORITHM active_algorithm;
 
 mem_free_block_t *next_free_next_fit;
 
+/* Function to get a free block using FIRST FIT algorithm */
 mem_free_block_t *get_free_block_first_fit(size_t requested_size) {
   mem_free_block_t *p_free_block = mvalloc_free_list_pointer;
   //check every next position while don't find enough one which is free
@@ -28,6 +30,7 @@ mem_free_block_t *get_free_block_first_fit(size_t requested_size) {
   return p_free_block;
 }
 
+/* Function to get a free block using BEST FIT algorithm */
 mem_free_block_t *get_free_block_best_fit(size_t requested_size) {
   mem_free_block_t *tmp = mvalloc_free_list_pointer;
   mem_free_block_t *best_one = NULL;
@@ -43,6 +46,7 @@ mem_free_block_t *get_free_block_best_fit(size_t requested_size) {
   return best_one;
 }
 
+/* Function to get a free block using WORST FIT algorithm */
 mem_free_block_t *get_free_block_worst_fit(size_t requested_size) {
   mem_free_block_t *p_iterator = mvalloc_free_list_pointer;
   mem_free_block_t *worst_fit_block = NULL;
@@ -58,6 +62,7 @@ mem_free_block_t *get_free_block_worst_fit(size_t requested_size) {
   return worst_fit_block;
 }
 
+/* Function to get a free block using NEXT FIT algorithm */
 mem_free_block_t *get_free_block_next_fit(size_t requested_size) {
   mem_free_block_t *p_iterator = next_free_next_fit;
   int flag = 0;
@@ -74,10 +79,10 @@ mem_free_block_t *get_free_block_next_fit(size_t requested_size) {
       p_iterator = mvalloc_free_list_pointer;
     }
   }
-
   return p_iterator;
 }
 
+/* Insert a few free block between two blocks*/
 void insert_free_block(mem_free_block_t *new_b, mem_free_block_t *left, mem_free_block_t *right) {
   if (right)
     right->prev = new_b;
@@ -89,6 +94,7 @@ void insert_free_block(mem_free_block_t *new_b, mem_free_block_t *left, mem_free
     mvalloc_free_list_pointer = new_b;
 }
 
+/* Delete a block */
 void delete_free_block(mem_free_block_t *block) {
   if (!block->prev)
     mvalloc_free_list_pointer = block->next;
@@ -99,6 +105,7 @@ void delete_free_block(mem_free_block_t *block) {
     block->next->prev = block->prev;
 }
 
+/* Split the current block into two blocks */
 void split_memory_block(mem_free_block_t *current, size_t desired_size) {
   mem_free_block_t *new_b =
       (mem_free_block_t *) ((unsigned char *) current + MAX(MIN_USED_BLOCK_SIZE + desired_size, MIN_FREE_BLOCK_SIZE));
@@ -112,6 +119,7 @@ void split_memory_block(mem_free_block_t *current, size_t desired_size) {
   }
 }
 
+/* Merge memory blocks to together */
 void merge_memory_blocks(mem_free_block_t *block) {
   if (block->prev
       && (unsigned char *) block == (unsigned char *) block->prev + block->prev->size + MIN_FREE_BLOCK_SIZE) {
@@ -133,8 +141,8 @@ void merge_memory_blocks(mem_free_block_t *block) {
 }
 
 void mavalloc_init(size_t size, enum ALGORITHM algorithm) {
-  // update size by minimum free block size
-  size += MIN_FREE_BLOCK_SIZE;
+  // update size by minimum free block size and minimum used block size
+  size += (5 * MIN_FREE_BLOCK_SIZE);
   mvalloc_free_list_pointer = malloc(size);
   mvalloc_free_list_pointer->size = size - MIN_FREE_BLOCK_SIZE;
   mvalloc_free_list_pointer->next = NULL;
@@ -160,25 +168,30 @@ void mavalloc_init(size_t size, enum ALGORITHM algorithm) {
 
 void *mavalloc_alloc(size_t size) {
 
+  /* Get a block of a given size. */
   mem_free_block_t *new_b = pf_get_free_block_t(size);
 
   if (!new_b) {
     fprintf(stderr, "%s", "Sufficiently large block cannot be allocated\n");
    return NULL;
   }
+
   mem_used_block_t *used_b = (mem_used_block_t *) new_b;
 
+  /* Split the block if it is too large. */
   if (new_b->size >= MAX(MIN_USED_BLOCK_SIZE + size, MIN_FREE_BLOCK_SIZE)) {
     split_memory_block(new_b, size);
     used_b->size = size;
-  } else {
-    delete_free_block(new_b);
+  }
+  else
+  {
 
+    delete_free_block(new_b);
     used_b->size = new_b->size + MIN_FREE_BLOCK_SIZE - MIN_USED_BLOCK_SIZE;
 
+    /*Update the pointers for the NEXT FIT Algorithm*/
     if (active_algorithm == NEXT_FIT) {
       mem_free_block_t *prev_next = new_b->next;
-
       if (prev_next)
         next_free_next_fit = prev_next;
       else if (mvalloc_free_list_pointer)
@@ -193,6 +206,7 @@ void *mavalloc_alloc(size_t size) {
   return used_b;
 }
 
+/* Free the previously allocated block */
 void mavalloc_free(void *p) {
   mem_free_block_t *new_b = (mem_free_block_t *) ((unsigned char *) p - MIN_USED_BLOCK_SIZE);
 
@@ -222,54 +236,25 @@ void mavalloc_free(void *p) {
     }
   }
 
+  /* if possible, merge two memory block to see if we can create a bigger block */
   merge_memory_blocks(new_b);
 }
 
+/* Get a block from the main memory of the requested size */
 size_t mavalloc_get_block_size(void *addr) {
   mem_used_block_t *used_b = (mem_used_block_t *) ((unsigned char *) addr - MIN_USED_BLOCK_SIZE);
   return used_b->size;
 }
 
-#if 0
-int main(int argc, char **argv) {
-  if (argc != 3) {
-    fprintf(stderr, "%s", "Wrong number of arguments\n");
-    exit(0);
-  }
-
-  size_t size = atoi(argv[1]);
-  enum ALGORITHM algorithm = atoi(argv[2]);
-
-
-  /*Initialize the memory for the my_malloc to fuction*/
-  mavalloc_init(ALIGN4(size), algorithm);
-
-  fprintf(stderr, "Memory : [%lu %lu] (%lu bytes)\n", (long unsigned int) mvalloc_heap_pointer,
-          (long unsigned int) (mvalloc_heap_pointer + MEMORY_SIZE), (long unsigned int) (MEMORY_SIZE));
-
-  int i;
-  for (i = 0; i < 10; i++) {
-    char *b = my_malloc_alloc(rand() % 8);
-    my_malloc_free(b);
-  }
-
-  char *a = my_malloc_alloc(1025);
-  my_malloc_free(a);
-
-  a = my_malloc_alloc(10);
-  my_malloc_free(a);
-
-  fprintf(stderr, "%lu\n", (long unsigned int) (my_malloc_alloc(9)));
-  return EXIT_SUCCESS;
-}
-#endif
-
+/* Free the main pointer */
 void mavalloc_destroy(void) {
   free(mvalloc_heap_pointer);
+  mvalloc_heap_pointer = NULL;
 }
 
+/* Function to get the number of active blocks */
 int mavalloc_size(void) {
-  int number_of_blocks = 0;
+  uint32_t number_of_blocks = 0;
   mem_free_block_t *p_iterator = mvalloc_heap_pointer;
   while (p_iterator)
   {
